65bc887b9bc6e1254d7dfc50237ff81dcf79360a
Mansur <funisinfinity26@gmail.com>
2021-07-23 07:49:03 +0600

    purchase distribution graphs

--- py_modules/admin.py
+++ py_modules/admin.py
@@ -3,6 +3,8 @@ from config import username, password
 from flask import current_app as app
 from flask import send_from_directory, Response
 
+from py_modules.mongo import users
+
 @app.route('/login')
 def send_file10():
 	return send_from_directory('./hidden/login/', 'index.html')
@@ -22,6 +24,15 @@ import random
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+def getData(u):
+	d = list(u.find())
+	output = []
+	for person in d:
+		for operation in person["operations"]:
+			if operation["details"] == 'кешбэк':
+				output.append(operation["sum"])
+	return output
+
 @app.route('/admin/<u>/<p>/analytic/plot.png')
 def plot_png(u, p):
 	if(u == username and p == password):
@@ -31,9 +42,8 @@ def plot_png(u, p):
 		return Response(output.getvalue(), mimetype='image/png')
 
 def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
-    axis.plot(xs, ys)
-    return fig
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	xs = getData(users)
+	axis.hist(xs, bins = int(180/5), color = 'blue', edgecolor = 'black')
+	return fig
