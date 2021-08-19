9211c8dd8836e9823631e1cb903e264586de7469
Hereugo <thereug6@gmail.com>
2021-08-19 19:22:21 +0600

    remove event listeners in popup

--- hidden/admin/message/message/script.js
+++ hidden/admin/message/message/script.js
@@ -95,16 +95,24 @@ const sendMessage = async () => {
     })
 }
 
+function removeAllEventListeners(node) {
+    var old_element = node;
+    var new_element = old_element.cloneNode(true);
+    old_element.parentNode.replaceChild(new_element, old_element);
+}
+
 function removePopup() {
     document.querySelector('#popup-container').style.visibility = 'hidden'
 }
 function popup(message, ok) {
     let container = document.querySelector('#popup-container')
+    let buttonOk = container.querySelector('button.ok')
     container.style.visibility = 'visible'
 
     container.querySelector('.popup-message').innerHTML = message
 
-    container.querySelector('button.ok').addEventListener('click', ok)
+    removeAllEventListeners(buttonOk)
+    buttonOk.addEventListener('click', ok)
 }
 
 (async () => {
