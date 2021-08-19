02ae8985a3b340626b5a03b755954709a5714529
Hereugo <thereug6@gmail.com>
2021-08-19 14:54:33 +0600

    better output in function

--- hidden/admin/message/message/script.js
+++ hidden/admin/message/message/script.js
@@ -1,13 +1,28 @@
-var phoneList = '["77476215825"]';
+async function recieveWhatsappPhoneList() {
+    var phoneList = []
+    await fetch("/mongodb/phones/whatsapp")
+    .then(r => r.text())
+    .then(r => {
+        r = r.split("'").join("\"").split("+").join("")
 
-fetch("/mongodb/phones/whatsapp")
-.then(r => r.text())
-.then(r => {
-    r = r.split("'").join("\"").split("+").join("")
-
-    phoneList = r
-    document.querySelector("details > div").innerText = r.split(",").join(",\n")
-})
+        phoneList = r
+        document.querySelector("details > div").innerText = r.split(",").join(",\n")
+    })
+    console.log(phoneList)
+    return phoneList
+}
+async function recieveTelegramPhoneList() {
+    var phoneList = []
+    await fetch("/mongodb/phones/telegram")
+    .then(r => r.text())
+    .then(r => {
+        r = r.split("'").join("\"")
+        phoneList = r
+    })
+    console.log(phoneList)
+    return phoneList
+}
+recieveTelegramPhoneList()
 
 const sendMessage = async () => {
     document.querySelector("button").disabled = true;
@@ -21,23 +36,23 @@ const sendMessage = async () => {
         },
         body: JSON.stringify({
             'message': message,
-            'phones': phoneList,
+            'phones': await recieveTelegramPhoneList(),
         })
     })
     if (request.ok) alert('Telegram messages were sent!')
     else alert('Telegram messages were not sent!!!, somethings wrong!')
 
-    // const whatsapp_url = `https://whatsapp-web-potato.herokuapp.com/mail/?numbers=${phoneList}&message=${message}`
-    // fetch(whatsapp_url)
-    // .then(async r => {
-    //     if(r.ok) {
-    //         alert("all good!")
-    //     }else{
-    //         alert(await r.text())
-    //     }
-    // })
-    // .catch(err => {
-    //     alert("Готово")
-    //     window.location.href = window.location.href;
-    // })
+    const whatsapp_url = `https://whatsapp-web-potato.herokuapp.com/mail/?numbers=${await recieveWhatsappPhoneList()}&message=${message}`
+    fetch(whatsapp_url)
+    .then(async r => {
+        if(r.ok) {
+            alert("all good!")
+        }else{
+            alert(await r.text())
+        }
+    })
+    .catch(err => {
+        alert("Готово")
+        window.location.href = window.location.href;
+    })
 }
