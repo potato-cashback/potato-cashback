2fb44c8ceea3c1d4a6a1c9bfb0cc4e4ceee1ad2c
Hereugo <thereug6@gmail.com>
2021-08-20 19:15:44 +0600

    changed url config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 URI = 'mongodb+srv://H_reugo:Nurmukhambetov@cluster0.vq2an.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-URL = 'https://potato-cashback.herokuapp.com'
+URL = 'https://test-potato-cashback.herokuapp.com'
 username = 'admin'
 password = 'admin'
