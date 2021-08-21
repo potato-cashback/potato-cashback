517e336ac59f030b906f9d3238c0d6f415d91151
Hereugo <thereug6@gmail.com>
2021-08-21 08:34:43 +0600

    Forgot to remove test ver

--- py_modules/mongo.py
+++ py_modules/mongo.py
@@ -7,7 +7,7 @@ import ssl
 
 try:
 	cluster = PyMongo(app, uri=URI, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
-	users = cluster.db.user_test
+	users = cluster.db.user
 
 	@app.route('/mongodb')
 	def get_data():
